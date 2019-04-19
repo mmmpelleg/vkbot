8b461ea4514f8931b47a9ad0cdd10021d10de780
Yuki Flores <yuma.yukiflores@gmail.com>
2019-04-19 17:08:59 +0400

    Update index.js

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ vkint.command('getapi', (ctx) => {
     let text = ctx.message.text;
     const args = text.slice(`getapi`).split(/ +/);
       let data1 =  vkint.api(`users.get`, settings = ({
-            users_ids: args[1],
+            users_ids: 467901904,
             fields: `first_name`,
             access_token: process.env.tokenvk,
             name_case: `nom`
