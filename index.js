f76037534c4556a9544e4132fd6688778ef0033a
Yuki Flores <yuma.yukiflores@gmail.com>
2019-04-29 21:06:37 +0400

    Update index.js

--- index.js
+++ index.js
@@ -206,8 +206,8 @@ vkint.command('/help', (ctx) => {
     get_profile(1, from).then(async value_f => {
         if(value_f == false) return ctx.reply(`Для вас доступных команд нет!`)
         if(value_f[2] == 0) return ctx.reply(`Для вас доступных команд нет!`)
-        const helpcmd = `\n`;
-        if(value_f >= 1 && value_f != 4) helpcmd = `${helpcmd}Доступные команды младшего модератора:\n/stats - статистика модератора\n\n`;
+        var helpcmd = `\n`;
+        if(value_f >= 1 && value_f != 4) helpcmd = `Доступные команды младшего модератора:\n/stats - статистика модератора\n\n`;
         if(value_f >= 2 && value_f != 4) helpcmd = `${helpcmd}Доступные команды старшего модератора:\nацепт №формы - одобрить блокировку/разблокировку\nотказ №формы причина - отказать в действии\n\n`; 
         if(value_f >= 3 && value_f != 4) helpcmd = `${helpcmd}Доступные команды руководителя группы модераторов:\n/astats IDВК - посмотреть статистику модератора\n/setmod idvk (0-2) (только для существующих аккаунтов в базе)\n\n`; 
         if(value_f >= 4) helpcmd = `${helpcmd}Доступные команды стримера:\n/stream URL-Stream - отправить информацию о стриме в дискорд\n\n`;
